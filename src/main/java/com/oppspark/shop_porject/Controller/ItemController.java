bd86fdade3837f85305e3c37290cd80727ac4d5f
N100-OppSpark <picks5@naver.com>
2024-04-04 20:33:26 +0000

    저장

--- src/main/java/com/oppspark/shop_porject/Controller/ItemController.java
+++ src/main/java/com/oppspark/shop_porject/Controller/ItemController.java
@@ -4,12 +4,10 @@ import com.oppspark.shop_porject.Entity.Item;
 import com.oppspark.shop_porject.Repository.ItemRepository;
 import com.oppspark.shop_porject.Service.ItemService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -18,17 +16,16 @@ public class ItemController {
 
     private final ItemRepository itemRepository;
     private final ItemService itemService;
+    private ItemRepository itemRepository1;
 
     @GetMapping("/list")
-    String list(Model model){List<Item> result = itemRepository.findAll();
-        model.addAttribute("items",result);
+    String list(Model model){
+        itemService.itemList(model);
         return "list.html";
     }
 
     @GetMapping("/write")
     String write(){
-
-
         return "write.html";
     }
 
@@ -45,6 +42,11 @@ public class ItemController {
         }
     }
 
+    @GetMapping("/edit/{id}")
+    String edit(Model model, @PathVariable Long id){
+        itemService.updateItem(model, id);
+        return "edit.html";
+    }
 
     @PostMapping("/api/item")
     String addItem(String title, Integer price){
@@ -54,6 +56,26 @@ public class ItemController {
         return "redirect:/list";
     }
 
+    @PutMapping("/api/edit/{id}")
+    String editItem(
+            String title,
+            Integer price,
+            @PathVariable Long id
+    ){
+        Item item = new Item();
+        item.setTitle(title);
+        item.setPrice(price);
+        item.setId(id);
+        
+        System.out.println("id = " + id);
+        System.out.println("title = " + title);
+        System.out.println("price = " + price);
+
+
+        itemRepository.save(item);
+
+        return "redirect:/list";
+    }
 }
 
 
--- src/main/java/com/oppspark/shop_porject/Service/ItemService.java
+++ src/main/java/com/oppspark/shop_porject/Service/ItemService.java
@@ -4,17 +4,34 @@ import com.oppspark.shop_porject.Entity.Item;
 import com.oppspark.shop_porject.Repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ItemService {
     private final ItemRepository itemRepository;
-    public void saveItem(String title, Integer price){
+    public void saveItem(
+            String title,
+            Integer price
+    ){
         Item item = new Item();
-
         item.setTitle(title);
         item.setPrice(price);
 
         itemRepository.save(item);
+    }
+
+    public void itemList(Model model){
+        List<Item> result = itemRepository.findAll();
+        model.addAttribute("items", result);
+    }
+
+    public void updateItem(Model model, Long id) {
+        Optional<Item> result = itemRepository.findById(id);
+        itemRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 아이템이 없습니다."));
+        model.addAttribute("item", result.get());
     }
 }
